340ae74a8b9d8d57bc13660dfd64da4bfc9101f5
Hernando17 <ginkotsu17@gmail.com>
2023-03-15 23:04:05 +0700

    layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,32 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('Layout/index')
+@section('title', 'Hernando | CV')
+@section('content')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hernando | CV</title>
-</head>
+    <h1>test</h1>
 
-<body>
-
-</body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                <a class="nav-link" href="#">Features</a>
-                <a class="nav-link" href="#">Pricing</a>
-                <a class="nav-link disabled">Disabled</a>
-            </div>
-        </div>
-    </div>
-</nav>
-
-</html>
+@endsection
