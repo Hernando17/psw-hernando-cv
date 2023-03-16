83b50d3163d9243fc8b202ace975fa730dd963dc
Hernando17 <ginkotsu17@gmail.com>
2023-03-16 23:08:12 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,14 @@
 @section('title', 'Hernando | CV')
 @section('content')
 
-    <h1>test</h1>
+    <div class="py-5">
+        <div class="bg-dark">
+            <div>
+                <h1 class="text-white">Welcome to my website</h1>
+                <span class="text-white">This is my portfolio website</span>
+            </div>
+        </div>
+        {{-- <img src="{{ asset('images/pas_foto.jpg') }}" class="w-25 h-25" /> --}}
+    </div>
 
 @endsection
