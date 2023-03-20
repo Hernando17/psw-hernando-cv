3a588fe6f23c6fa9439cc7de648c9fdaeb72e842
Hernando17 <ginkotsu17@gmail.com>
2023-03-20 22:43:09 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,10 +5,10 @@
     <div>
         <div class="d-flex align-items-center mt-5 py-3">
             <div class="d-flex container">
-                <div class="d-flex w-100 align-items-center flex-column flex-sm-row">
+                <div class="d-flex w-100 gap-md-5 align-items-center align-items-md-start flex-column flex-sm-row">
                     <img src="{{ asset('images/pas_foto.jpg') }}" class="foto rounded-5" />
                     <div>
-                        <div>
+                        <div class="mt-5">
                             <h1 class="fw-bold">Welcome to my website</h1>
                             <div class="mt-5">
                                 <h2>Hernando</h2>
