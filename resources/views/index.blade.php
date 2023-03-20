c9c399b9a7bd1d5a9c4d81a43d28cee508e8c569
Hernando17 <ginkotsu17@gmail.com>
2023-03-20 22:40:19 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,14 +2,24 @@
 @section('title', 'Hernando | CV')
 @section('content')
 
-    <div class="py-5">
-        <div class="bg-dark">
-            <div>
-                <h1 class="text-white">Welcome to my website</h1>
-                <span class="text-white">This is my portfolio website</span>
+    <div>
+        <div class="d-flex align-items-center mt-5 py-3">
+            <div class="d-flex container">
+                <div class="d-flex w-100 align-items-center flex-column flex-sm-row">
+                    <img src="{{ asset('images/pas_foto.jpg') }}" class="foto rounded-5" />
+                    <div>
+                        <div>
+                            <h1 class="fw-bold">Welcome to my website</h1>
+                            <div class="mt-5">
+                                <h2>Hernando</h2>
+                                <h4>Student</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        {{-- <img src="{{ asset('images/pas_foto.jpg') }}" class="w-25 h-25" /> --}}
     </div>
+
 
 @endsection
